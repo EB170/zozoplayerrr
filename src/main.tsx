d55442a79a9f492c7aecaff01456f49277ed17f7
[dyad] <17000eb@gmail.com>
2025-11-10 16:39:00 +0100

    [dyad] Removing push notifications, keeping pop-under - wrote 2 file(s), deleted 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Enregistrement manuel du Service Worker pour les notifications push
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('Service Worker enregistré avec succès:', registration.scope);
-      })
-      .catch(error => {
-        console.error('Échec de l\'enregistrement du Service Worker:', error);
-      });
-  });
-}
-
 createRoot(document.getElementById("root")!).render(<App />);
